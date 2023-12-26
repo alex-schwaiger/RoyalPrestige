af92abb06e43f0d987f787dbc86dde54b2784358
alex-schwaiger <schwaiger.alexander@gmx.at>
2023-12-26 14:06:07 +0100

    Initial Commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Image src="/royal-prestige-logo.png" alt="Royal Prestige" width={500} height={500} />
+      </div>
+    </div>
+  )
+}
