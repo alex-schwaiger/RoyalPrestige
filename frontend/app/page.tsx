09096fadb3591e1f974f4d0d47eb8bb47494fe92
alex-schwaiger <schwaiger.alexander@gmx.at>
2023-12-26 14:12:43 +0100

    added Button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Image src="/royal-prestige-logo.png" alt="Royal Prestige" width={500} height={500} />
       </div>
+      <Button>Click me</Button>
     </div>
   )
 }
